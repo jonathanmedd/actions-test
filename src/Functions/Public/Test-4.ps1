6e7734c4cce132aba32532d73a0e84e9164970a2
Jonathan Medd <jon_medd@hotmail.com>
2021-02-10 12:22:41 +0000

    Added Test-4

---
+++ src/Functions/Public/Test-4.ps1
@@ -0,0 +1,24 @@
+function Test-4 {
+    <#
+    .SYNOPSIS
+        Test-4
+    .DESCRIPTION
+        Test-4
+    .PARAMETER Name
+        Name
+    .EXAMPLE
+        Test-4 -Name 'General Kenobi'
+    .INPUTS
+        System.String
+    .OUTPUTS
+        System.String
+    #>
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Name
+    )
+
+        Write-Output "$Name you were supposed to be the chosen one"
+}
